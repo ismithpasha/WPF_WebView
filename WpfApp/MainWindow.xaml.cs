573af828f48e76d99a0856a61082c7cc112ba55a
ismithpasha <ismithpasha@gmail.com>
2024-06-06 00:23:54 +0600

    Init WPF Web

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWebView(); 
+        }
+
+        private async void InitializeWebView()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.Navigate("http://localhost:3001/");
+
+        } 
+      
+    }
+}
